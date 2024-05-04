42b0f4e856d6feae803db2bb26c9330ee39d38d8
Ali Bouzar <ali.bouzar@gmail.com>
2024-05-03 20:43:14 -0500

    first commit contains the backend project with initial configuration

---
+++ src/test/java/net/miaoubich/NetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.miaoubich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
